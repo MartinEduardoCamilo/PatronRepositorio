9f763273a800d1e30f639aec32b5b79e66870162
MartinCamilo13 <47596798+MartinCamilo13@users.noreply.github.com>
2019-10-03 21:48:48 -0400

    Update

---
+++ PatronRepositorio/PatronRepositorio/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using PatronRepositorio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.DAL
+{
+    public class Contexto : DbContext
+    {
+        public  DbSet<Empleados> Empleados { get; set; }
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
---
+++ PatronRepositorio/PatronRepositorio/Entidades/Empleados.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class Empleados
+    {
+        [Key]
+        public int EmpleadoID { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Cedula { get; set; }
+        public decimal sueldo { get; set; }
+        public decimal Incentivo { get; set; }
+
+        public Empleados() 
+        {
+            EmpleadoID = 0;
+            Fecha = DateTime.Now;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+            Celular = string.Empty;
+            Cedula = string.Empty;
+            sueldo = 0;
+            Incentivo = 0;
+        }
+    }
+}
